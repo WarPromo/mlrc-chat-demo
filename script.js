7d60d0c5d9bdb5248665894850bdbaa7ddaff9d9
WarPromo <kooltyme1@gmail.com>
2023-11-14 21:53:51 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,198 @@
+let hovers = ["Math Bot", "Superhero Bot", "Conversation Bot"]
+let messages = ["Give me an equation.", "I am superman.", "Chat with me!"]
+let ids = ["equationsolver", "chatgpt", "huggingface"]
+
+let hoverelement = document.getElementById("dropdownhover");
+
+
+let currentbot = 0;
+let currentchats = []
+
+let menuopen = false;
+
+let hoversquares = document.getElementsByClassName("dropdownsquare")
+
+let server = "http://127.0.0.1:5000";
+
+function init(){
+  for(var i = 0; i < ids.length; i++){
+
+    console.log("yo blud");
+
+    let messageholder = document.createElement("div");
+    messageholder.classList.add("innermessages");
+    messageholder.style.display = "none";
+    currentchats.push(messageholder);
+
+    if(i == 0){
+      messageholder.style.display = "";
+    }
+
+    let title = document.createElement("p");
+    title.classList.add("messagesectiontitle");
+    title.innerHTML = hovers[i];
+    messageholder.appendChild(title);
+
+    recieveMessage({bot: i, message: messages[i]})
+
+    document.getElementById("messages").appendChild(messageholder);
+
+  }
+}
+
+
+
+function scrollbottom(){
+
+  let messageContainer = document.getElementById("messages");
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+
+}
+
+
+function sendmessageserver(bot, content){
+
+  console.log("sending message to server...", bot, content);
+
+  fetch(server + "/sendmessage", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(
+          {
+            "bot": bot,
+            "content": content
+          }
+      )
+  }
+  ).then(res => {
+    return res.json()
+  }).then(json => {
+    setTimeout(() => {
+      recieveMessage(json);
+      scrollbottom();
+    }, 1000)
+  })
+
+
+}
+
+
+function recieveMessage(data){
+
+  console.log(data);
+
+  let bot = data.bot;
+  let message = data.message;
+  let messageObj = sendmessage(hovers[bot], message, "lightblue");
+  let myid = bot;
+  currentchats[myid].removeChild(currentchats[myid].lastChild)
+  currentchats[myid].appendChild(messageObj);
+
+  scrollbottom();
+
+
+}
+
+console.log("hello");
+
+let botmessage = null;
+
+
+function changechat(element){
+  console.log("called");
+  let indicator = document.getElementById("dropdownhover");
+
+  let myid = ids.indexOf(element.id);
+  indicator.innerHTML = hovers[myid];
+  currentbot = myid;
+
+  for(var i = 0; i < currentchats.length; i++){
+    currentchats[i].style.display = i == myid ? "" : "none";
+  }
+
+  let child = element;
+  let before = element.parentElement.children[0];
+  let parent = element.parentElement;
+
+  if(before != child){
+    parent.removeChild(child);
+    parent.insertBefore(child, before);
+  }
+
+
+
+  hovermenu(false);
+}
+
+
+function sendusermessage(author, message){
+
+  let myobj = sendmessage(author, message, "red");
+
+  currentchats[currentbot].appendChild(myobj);
+  scrollbottom();
+
+  sendmessageserver(currentbot, message);
+  scrollbottom();
+
+}
+
+function sendmessage(author, message, color="gray"){
+
+  let messagevar = document.createElement("p");
+
+  messagevar.classList.add("usermessage")
+
+  messagevar.innerHTML = `<span style="font-weight: bold; color: ${color}">${author}:</span> ${message}`;
+
+  return messagevar;
+
+}
+
+
+function sendinput(e, bypass=false){
+
+  console.log("yo");
+
+  if(e.keyCode == 13 || bypass){
+
+    console.log("here");
+
+    let inputField = document.getElementById("messageinput");
+    let content = inputField.value;
+
+    if(inputField.value.length == 0) return;
+
+    inputField.value = "";
+    sendusermessage("User", content);
+    setTimeout(() => {
+      let element = sendmessage(hovers[currentbot], "is typing...");
+      element.style.opacity = 0.5;
+      currentchats[currentbot].appendChild(element)
+      scrollbottom();
+    }, 300)
+
+    //inputField.disabled = true;
+
+  }
+
+}
+
+
+
+function hovermenu(showmenu){
+
+  let menu = document.getElementById("dropdownmenu")
+
+  let indicator = document.getElementById("dropdownhover");
+
+  if(showmenu){
+    menu.style.display = "";
+    indicator.style.display = "none";
+  }
+  else{
+    menu.style.display = "none";
+    indicator.style.display = "";
+  }
+
+}
---
+++ server.py
@@ -0,0 +1,177 @@
+from flask import Flask, json
+from flask import request
+from flask_cors import CORS, cross_origin
+#from transformers import pipeline
+
+import openai;
+openai.api_key = ""
+gpt_conversation = "You are Superman, act like it. Start your first message with 'I am superman!' or something cliche like that.";
+
+regular_conversation = "Talk casually with the user."
+
+messages = [[],[],[]]
+message_names = ["Equation Solver", "ChatGPT", "HuggingFace"];
+
+api = Flask(__name__)
+cors = CORS(api)
+api.config['CORS_HEADERS'] = 'Content-Type'
+
+#huggingface_bot = pipeline("text-generation", model = 'gpt2')
+
+
+@api.route('/sendmessage', methods=['POST'])
+@cross_origin()
+def process_message():
+
+  data = request.get_json(force=True);
+
+  bot = data["bot"];
+  message = data["content"]
+  messages[bot].append(message)
+
+  if bot == 0:
+      answer = "I don't know"
+
+      try:
+          answer = process_equationsolver(message)
+      except:
+          print("Error with input");
+
+      return json.dumps({
+
+        "bot": bot,
+        "message": process_equationsolver(message)
+
+      });
+
+  if bot == 1:
+       return json.dumps({
+
+         "bot": bot,
+         "message": process_gpt(message)
+
+       });
+
+  if bot == 2:
+      return json.dumps({
+
+        "bot": bot,
+        "message": process_regular(message)
+
+      })
+
+
+
+def process_gpt(message):
+    global gpt_conversation;
+    print(gpt_conversation);
+
+    gpt_conversation += "\nUser: " + message;
+    gpt_conversation += "\nSuperman: "
+
+    chat = openai.Completion.create(
+        model="text-davinci-003", prompt=gpt_conversation, max_tokens=1024, stop=None,n=1
+    )
+
+    reply = chat.choices[0].text.strip()
+
+    gpt_conversation += reply;
+
+    return reply;
+
+def process_regular(message):
+    global regular_conversation;
+    print(regular_conversation);
+
+    regular_conversation += "\nUser: " + message;
+    regular_conversation += "\nSuperman: "
+
+    chat = openai.Completion.create(
+        model="text-davinci-003", prompt=regular_conversation, max_tokens=1024, stop=None,n=1
+    )
+
+    reply = chat.choices[0].text.strip()
+
+    regular_conversation += reply;
+
+    return reply;
+
+def process_equationsolver(message):
+
+    answer = number_compute(message);
+
+    if answer == None:
+        return "I don't know"
+
+    if type(answer) is bool:
+
+        if bool == True:
+            return "The statement is true."
+        else:
+            return "The statement is false."
+    else:
+        return "The answer is " + str(answer) + "."
+
+
+
+
+def number_compute(question):
+
+    numbers = "1234567890";
+
+
+    first_occurance = -1;
+    last_occurance = -1;
+
+    input = "";
+
+    for i in range(len(question)):
+        if question[i] in numbers:
+            if first_occurance == -1:
+                first_occurance = i;
+                last_occurance = i;
+            else:
+                last_occurance = i;
+
+    for j in range(first_occurance, last_occurance + 1):
+        input += question[j];
+
+    if "=" in input:
+        parts = input.split("=")
+        return int(parts[1]) == simple_solve(parts[0]);
+    else:
+        return simple_solve(input)
+
+def simple_solve(equation):
+
+    operations = "*-+^/x"
+
+    for char in operations:
+
+        if char in equation:
+
+            parts = equation.split(char);
+            num1 = int(parts[0]);
+            num2 = int(parts[1]);
+
+            if char == "*" or char == "x":
+                return num1 * num2;
+
+            if char == "/":
+                return num1 / num2;
+
+            if char == "-":
+                return num1 - num2;
+
+            if char == "+":
+                return num1 + num2;
+
+            if char == "^":
+                return num1 ** num2;
+
+
+
+
+
+if __name__ == '__main__':
+    api.run()
